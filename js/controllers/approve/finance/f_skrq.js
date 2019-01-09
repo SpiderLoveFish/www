955f8909482b4c0eb8aa30dfa8d5f6bc0e1b40f6
Spider00153 <spider00153@hotmail.com>
2019-01-09 19:54:43 +0800

    增加收款确认

---
+++ js/controllers/approve/finance/f_skrq.js
@@ -0,0 +1,129 @@
+var list = document.getElementById("list");
+var starIndex = 10;
+var endIndex = 1000;
+var selecttype = common.getQueryString("selecttype");
+var status = "999";
+
+var html_CanYu = '<a href="javascript:;"  class="sc_cell sc_padding mui-table-view-cell" id="@ID">' +
+	//	'		<div class="sc_cell_hd sc_pic_txt"><img src="@IsHostPic"></div>' +
+	'		<div class="sc_cell_bd sc_cell_primary">' +
+	'			<p>【客户】@skkh</p>' +
+	'			<p class="label_describe_2">@dz</p>' +
+	'			<span class="sc_comment"></span>' +
+	'			<span class="sc_comment">@SContext-@ReleaseTime</span>' +
+	'		</div>' +
+	'		<div class="sc_cell_data">' +
+	'			<p></br></p>' +
+	'		</div>' +
+	
+	'	</a>';
+
+function getpullupRefresh() {
+	setTimeout(function() {
+		getquestionnairelist();
+	}, 500);
+}
+
+function ChangeDateFormat(jsondate) {
+	jsondate = jsondate.replace("/Date(", "").replace(")/", "");
+	if(jsondate.indexOf("+") > 0) {
+		jsondate = jsondate.substring(0, jsondate.indexOf("+"));
+	} else if(jsondate.indexOf("-") > 0) {
+		jsondate = jsondate.substring(0, jsondate.indexOf("-"));
+	}
+
+	var date = new Date(parseInt(jsondate, 10));
+	var month = date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1;
+	var currentDate = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+
+	return date.getFullYear() +
+		"年" +
+		month +
+		"月" +
+		currentDate +
+		"日";
+	//  + " "
+	//  + date.getHours()
+	//  + ":"
+	//  + date.getMinutes();
+}
+var search = document.getElementById("search");
+document.getElementById('search').addEventListener('input', function() {
+	list.innerHTML = "";
+	starIndex=10;
+	getquestionnairelist();
+	}, false);
+
+function getquestionnairelist() {
+	// selecttype = common.getQueryString("selecttype");	
+	if(selecttype == "cgdsh") status = 1;
+	else if(selecttype == "cgdqr") status = 2;
+	var data = { 
+		nowindex: starIndex, 
+		strWhere: search.value//' and isNode=' + status + '',  
+	};
+	//alert(JSON.stringify(data))
+	common.postApi('GetCRM_receive', data, function(response) {
+		dataArray = eval(response.data);
+		//alert(JSON.stringify(response.data))
+		for(var i = 0; i < dataArray.length; i++) {
+			// alert(i)
+			var obj = dataArray[i];
+			//if (obj.DoPerson == getUserInfo().ID) {
+			list.innerHTML += html_CanYu.replace('@ID', obj.id).replace('@skkh', obj.Customer).replace('@dz', substringAddPoint(obj.address, 15)).replace('@ReleaseTime', ChangeDateFormat(obj.create_date)).replace('@SContext',obj.create_name);
+			//			} else {
+			//				list.innerHTML += html_No.replace('@IsHostPic', obj.IsHostPic).replace('@ID', obj.id).replace('@STheme', obj.BudgetName).replace('@SContext', substringAddPoint(obj.address, 15)).replace('@ReleaseTime', ChangeDateFormat(obj.DoTime));
+			//			}
+		}
+		starIndex = starIndex + 10;
+		mui('#pullrefresh').pullRefresh().endPullupToRefresh((dataArray.length < 10)); //参数为true代表没有更多数据了
+
+	}, 'json');
+}
+var detailPage = null;
+mui.plusReady(function() {
+	mui('#list').on('tap', '.sc_cell', function(e) {
+		var id = this.getAttribute('id');
+		var type = common.getQueryString("selecttype");
+		//var webview = common.getTemplate('page1');
+		var template = common.getTemplate('f_skrqdetail', 'f_skqrdetail.html?id=' + id );
+		//		webview.loadURL('ysdetail.html?id=' + id+'&selecttype='+type);
+	});
+	if(plus.os.name != "Android") {
+		var pullrefresh = document.getElementById("pullrefresh");
+		pullrefresh.style.marginTop = CommonTop;
+	}
+	mui.init({
+		pullRefresh: {
+			container: '#pullrefresh',
+			up: {
+				contentrefresh: '正在加载...',
+				callback: getpullupRefresh
+			}
+		}
+	});
+	if(mui.os.plus) {
+		mui.plusReady(function() {
+
+			setTimeout(function() {
+				mui('#pullrefresh').pullRefresh().pullupLoading();
+			}, 50);
+		});
+
+	} else {
+		mui.ready(function() {
+			mui('#pullrefresh').pullRefresh().pullupLoading();
+		});
+
+	}
+
+	window.addEventListener('refresh1', function(e) {
+		//alert( e.detail.type);
+		selecttype = e.detail.type;
+		starIndex = 10;
+		list.innerHTML = "";
+		getquestionnairelist();
+
+	});
+
+});
---
+++ js/controllers/approve/finance/f_skrqdetail.js
@@ -0,0 +1,104 @@
+mui.init();
+var list = document.getElementById("list");
+var starIndex = 10;
+var endIndex = 1000;
+var selecttype = 'dqr';
+var id = common.getQueryString("id");
+var type= common.getQueryString("selecttype");
+var commitLock = true; 
+var status = 0;//1确认 7作废
+var html_CanYu =
+ 	'<a href="javascript:;"  class="sc_cell sc_padding mui-table-view-cell" id="@ID" tag="@tag">' +
+	//	'		<div class="sc_cell_hd sc_pic_txt"><img src="@IsHostPic"></div>' +
+	'		<div class="sc_cell_bd sc_cell_primary">' +
+	'			<p>@STheme</p>' +
+	'			<p class="label_describe_2">@SContext</p>' +
+	'			<span class="sc_comment">@ReleaseTime</span>' +
+	'		</div>' +
+	'		<div class="sc_cell_data">' +
+	'			<p>@flag</p>' +
+	'		</div>' +
+	'	</a>';
+mui.plusReady(function() {
+	//后退键隐藏层
+	mui.back = function() {
+		var fatherView = plus.webview.currentWebview().opener(); //父页面
+		mui.fire(fatherView, 'refresh1', {type: type});
+		common.currentWebviewHide();
+	};
+ 
+	var data = {
+		id: id
+	};
+	//alert(starIndex)
+	common.postApi('GetCRM_receiveDetail', data, function(response) {
+		dataArray = eval(response.data);
+		//alert(JSON.stringify(response))
+		for(var i = 0; i < dataArray.length; i++) {
+			var obj = dataArray[i];
+		 
+			list.innerHTML += html_CanYu.replace('@ID', obj.id).replace('@tag', obj.Customer_id).replace('@STheme', obj.Customer).replace('@SContext', obj.address).replace('@ReleaseTime', obj.Pay_type).replace('@flag', obj.receive_real);
+			//			} else {
+			//				list.innerHTML += html_No.replace('@IsHostPic', obj.IsHostPic).replace('@ID', obj.id).replace('@STheme', obj.BudgetName).replace('@SContext', substringAddPoint(obj.address, 15)).replace('@ReleaseTime', ChangeDateFormat(obj.DoTime));
+			//			}
+		}
+		 
+		//mui('#pullrefresh').pullRefresh().endPullupToRefresh((dataArray.length < 10)); //参数为true代表没有更多数据了。
+		 
+	}, 'json');
+ 
+
+	 	var btnState = document.getElementById("btnState");
+	//审批通过
+	btnState.addEventListener("tap", function(e) { 
+		 status = 1;//1确认 7作废
+		e.detail.gesture.preventDefault(); //修复iOS 8.x平台存在的bug，使用plus.nativeUI.prompt会造成输入法闪一下又没了
+		var btnArray = ['确定', '取消'];
+		mui.confirm('确认操作该条记录？', '同意', btnArray, function(e) {
+			if(e.index == 0) {
+				approve(status);
+			}
+		});
+
+	});
+var btnNopass = document.getElementById("btnNopass");
+	//审批不通过
+	btnNopass.addEventListener("tap", function(e) { 
+		  status = 7;//1确认 7作废
+		e.detail.gesture.preventDefault(); //修复iOS 8.x平台存在的bug，使用plus.nativeUI.prompt会造成输入法闪一下又没了
+		var btnArray = ['确定', '取消'];
+		mui.confirm('确认作废条记录？', '不同意', btnArray, function(e) {
+			if(e.index == 0) {
+				approve(status);
+			}
+		});
+	});
+
+	//审批
+	function approve(zt) {
+		if(!commitLock) {
+			return;
+		}
+			var data = {
+			status: zt,
+			id: id
+		};
+		commitLock = false;
+		//common.showWaiting(); 
+		//alert(JSON.stringify(data))
+		common.postApi('UpdateCRM_receiveStatus', data, function(response) {
+			if(response.data == "success") {
+				common.toast("提交成功");
+				mui.back();
+			} else {
+				common.toast("服务器异常，请稍候重试..");
+			}
+			commitLock = true;
+			common.closeWaiting();
+		}, 'json');
+	}
+
+	
+ 
+
+});
